e6cb49fcdcef28d43b309ab14f5e8bf699f5d477
lufte <javierayres@gmail.com>
2014-10-23 20:42:10 -0200

    First commit

---
+++ game.js
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2013, Javier Ayres
+ * 
+ * This file is part of Lona.
+ * 
+ * Lona is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Lona is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Lona. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//Snake constants
+var SPEED = 0.003; //Rotation speed, in radians/ms.
+var DISTANCE = 0.5; //Distance between body parts, in radians.
+var RADIUS, LINEAR_WIDTH, CIRCULAR_WIDTH;
+
+//Display constants
+var SCREEN_WIDTH, SCREEN_HEIGHT, FOCI_X ,FOCUS_1_Y, FOCUS_2_Y, BORDER_WIDTH;
+var MAX_SCORE_KEY = 'hs';
+
+//Global snake variables
+var centers, item, rotDiff, now, lastRepaint, score, isPaused;
+//Global screen variables
+var container, canvas, svg, ellipse, ctx, scoreSpan, pauseSpan, maxSpan, gameOverContainer, gameOverMessage;
+
+function initScreen() {
+    container = document.getElementById('container');
+    canvas = document.getElementById('c');
+    svg = document.getElementById('s');
+    ellipse = document.getElementById('e');
+    scoreSpan = document.getElementById('score');
+    pauseSpan = document.getElementById('pause');
+    maxSpan = document.getElementById('max');
+    gameOverContainer = document.getElementById('game-over-container');
+    gameOverMessage = document.getElementById('game-over');
+    
+    //Get support for touch events
+    if (!!('ontouchstart' in window)) {
+        document.body.addEventListener('touchstart', tap, false);
+    } else {
+        document.body.addEventListener('click', tap, false);
+    }
+    
+    //Get support of requestAnimationFrame
+    if (typeof requestAnimationFrame == 'undefined') {
+        window.requestAnimationFrame = mozRequestAnimationFrame;
+    }
+    
+    //Get window size
+    if (window.innerWidth / window.innerHeight > 2 / 3) {
+        SCREEN_HEIGHT = window.innerHeight;
+        SCREEN_WIDTH = Math.round(window.innerHeight * 2 / 3);
+        //canvas.style.left = Math.round((window.innerWidth - SCREEN_WIDTH) / 2) + 'px';
+        //svg.style.left = Math.round((window.innerWidth - SCREEN_WIDTH) / 2) + 'px';
+    } else {
+        SCREEN_WIDTH = window.innerWidth;
+        SCREEN_HEIGHT = Math.round(window.innerWidth * 3 / 2);
+    }
+    BORDER_WIDTH = Math.round(SCREEN_WIDTH / 64);
+    //Set lona's constants based on screen dimentions
+    RADIUS = Math.round(SCREEN_WIDTH / 12.8);
+    LINEAR_WIDTH = Math.round(SCREEN_WIDTH / 13.3);
+    CIRCULAR_WIDTH = LINEAR_WIDTH / (RADIUS * 2);
+    
+    //Set canvas size
+    container.style.width = SCREEN_WIDTH + 'px';
+    container.style.height = SCREEN_HEIGHT + 'px';
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    svg.setAttribute('width', SCREEN_WIDTH);
+    svg.setAttribute('height', SCREEN_HEIGHT);
+    ellipse.setAttribute('cx', SCREEN_WIDTH / 2);
+    ellipse.setAttribute('cy', SCREEN_HEIGHT / 2);
+    ellipse.setAttribute('rx', SCREEN_WIDTH / 2 - BORDER_WIDTH);
+    ellipse.setAttribute('ry', SCREEN_HEIGHT / 2 - BORDER_WIDTH);
+    ellipse.setAttribute('stroke-width', BORDER_WIDTH * 2);
+    document.body.style.fontSize = Math.round(SCREEN_WIDTH / 8) + 'px';
+    
+    //Shrink SCREEN_WIDTH and SCREEN_HEIGHT to take the ellipse's border into account
+    SCREEN_WIDTH -= BORDER_WIDTH * 4;
+    SCREEN_HEIGHT -= BORDER_WIDTH * 4;
+    
+    //Set ellipse's centers
+    FOCI_X = Math.round(SCREEN_WIDTH / 2) + BORDER_WIDTH * 2;
+    //f² = a² - b²
+    FOCUS_1_Y = SCREEN_HEIGHT / 2 - Math.sqrt(Math.pow((SCREEN_HEIGHT - LINEAR_WIDTH) / 2, 2) - Math.pow((SCREEN_WIDTH - LINEAR_WIDTH) / 2, 2)) + BORDER_WIDTH * 2;
+    FOCUS_2_Y = SCREEN_HEIGHT / 2 + Math.sqrt(Math.pow((SCREEN_HEIGHT - LINEAR_WIDTH) / 2, 2) - Math.pow((SCREEN_WIDTH - LINEAR_WIDTH) / 2, 2)) + BORDER_WIDTH * 2;
+    
+    isPaused = false;
+    if(typeof(Storage) !== "undefined") {
+        max.innerHTML = localStorage.getItem(MAX_SCORE_KEY);
+        if (max.innerHTML == '') {
+            max.innerHTML = '0';
+        }
+    }
+    
+    ctx = canvas.getContext('2d');
+    ctx.lineWidth = LINEAR_WIDTH;
+    ctx.strokeStyle = '#FFDB21';
+    ctx.fillStyle = '#FFDB21';
+    ctx.lineCap = 'round';
+    
+    go();
+}
+
+function go() {
+    centers = new Array(new Center(Math.round(SCREEN_WIDTH / 2), Math.round(SCREEN_HEIGHT / 2), 1, 0, DISTANCE * 9));
+    item = new Item();
+    lastRepaint = new Date().getTime();
+    score = 0;
+    scoreSpan.innerHTML = score + '';
+    requestAnimationFrame(loop);
+}
+
+function loop() {
+    if (!isPaused) {
+        now = new Date().getTime();
+        rotDiff = SPEED * (now - lastRepaint);
+        ctx.clearRect(0, 0, SCREEN_WIDTH + BORDER_WIDTH * 4, SCREEN_HEIGHT + BORDER_WIDTH * 4);
+        centers[centers.length - 1].moveStart(rotDiff);
+        if (item.isDrawn() && centers[centers.length - 1].collide(item.getPosition())) {
+            item.reset();
+            centers[0].grow();
+            score++;
+            scoreSpan.innerHTML = score + '';
+            if (parseInt(max.innerHTML) < score) {
+                max.innerHTML = scoreSpan.innerHTML;
+            }
+        }
+        item.draw();
+        for (var i = 0; i < centers.length && rotDiff > 0; i++) {
+            rotDiff = centers[i].moveEnd(rotDiff);
+            if (centers[i].mustShift()) {
+                centers.shift();
+            }
+        }
+        var collision = false;
+        var offBorders = false;
+        var currentLength = 0;
+        for (var j = centers.length - 1; j >= 0; j--) {
+            currentLength += centers[j].getLength();
+            if (currentLength >= 2 * Math.PI - 2 * CIRCULAR_WIDTH && !collision) {
+                if (j == centers.length - 1) {
+                    collision = centers[j].collideSelf();
+                    offBorders = centers[j].isOffBorders();
+                } else {
+                    collision = centers[j].collide(centers[centers.length - 1].getEndPosition());
+                }
+            }
+            if (j == centers.length - 1) {
+                offBorders = centers[j].isOffBorders();
+            }
+            centers[j].draw();
+        }
+        if (collision || offBorders) {
+            gameOver();
+            if (score > localStorage.getItem(MAX_SCORE_KEY)) {
+                localStorage.setItem(MAX_SCORE_KEY, score);
+            }
+        } else {
+            requestAnimationFrame(loop);
+            lastRepaint = now;
+        }
+    }
+}
+
+function tap(event) {
+    if (!isPaused) {
+        if (event.target.id != 'pause' && event.target.id != 'about') {
+            var last = centers[centers.length - 1];
+            centers.push(
+                new Center(
+                    last.x + Math.cos(last.direction > 0 ? last.endRot : last.startRot) * 2 * RADIUS,
+                    last.y + Math.sin(last.direction > 0 ? last.endRot : last.startRot) * 2 * RADIUS,
+                    -last.direction,
+                    (Math.PI + (last.direction > 0 ? last.endRot : last.startRot)),
+                    (Math.PI + (last.direction > 0 ? last.endRot : last.startRot))
+                )
+            );
+        }
+    }
+}
+
+function normalizeAngle(angle) {
+	return angle >= 0 ? angle % (Math.PI * 2) : angle % (Math.PI * 2) + (Math.PI * 2);
+}
+
+function pause() {
+    if (isPaused) {
+        isPaused = false;
+        lastRepaint = new Date().getTime();
+        pauseSpan.setAttribute('class', 'icon-pause');
+        loop();
+    } else {
+        isPaused = true;
+        pauseSpan.setAttribute('class', 'icon-play');
+        //⟳
+    }
+}
+
+function showAbout() {
+    window.location = 'about.html';
+}
+
+function gameOver() {
+    gameOverContainer.style.visibility = 'visible';
+    gameOverMessage.style.visibility = 'visible';
+    gameOverContainer.style.opacity = 0.3;
+    gameOverMessage.style.opacity = 1;
+}
+
+function restart() {
+    gameOverContainer.style.visibility = 'hidden';
+    gameOverMessage.style.visibility = 'hidden';
+    gameOverContainer.style.opacity = 0;
+    gameOverMessage.style.opacity = 0;
+    go();
+}
